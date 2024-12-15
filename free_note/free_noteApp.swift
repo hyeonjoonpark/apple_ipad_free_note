bf3cf34a3996cd1071ecc0de2f68afcf74d86f16
hyeonjoonpark <pjjoon1379@gmail.com>
2024-12-16 00:03:32 +0900

    Initial Commit

---
+++ free_note/free_noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  free_noteApp.swift
+//  free_note
+//
+//  Created by 박현준 on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct free_noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ free_noteTests/free_noteTests.swift
@@ -0,0 +1,17 @@
+//
+//  free_noteTests.swift
+//  free_noteTests
+//
+//  Created by 박현준 on 12/16/24.
+//
+
+import Testing
+@testable import free_note
+
+struct free_noteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
